832dc420e6097b614bfd83d50f1086561e4beac1
Rajan Singh <singh.rajan3108@gmail.com>
2022-02-14 22:36:39 +0530

    second

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log("hello");
+//Get Quotes API
+
+const newQuote = document.querySelector("#new-quote");
+const quoteText = document.querySelector("#quote");
+const author = document.querySelector("#author");
+const twitter = document.querySelector("#twitter");
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+newQuote.addEventListener("click", async () => {
+  const quoteData = await fetch("https://type.fit/api/quotes");
+  const data = await quoteData.json();
+  console.log(data);
+  const maxNumber = getRandomInt(data.length);
+  console.log(maxNumber);
+  //  quoteText.innerHTML = "";
+  quoteText.textContent = data[maxNumber].text;
+  if (data[maxNumber].author) {
+    author.textContent = data[maxNumber].author;
+  } else {
+    author.textContent = "Unknown";
+  }
+});
+
+let apiQuotes = [];
+async function getQuotes() {
+  const Url = "https://type.fit/api/quotes";
+  try {
+    if (!response.ok) alert(`${response.data.message}`);
+    const response = await fetch(Url);
+    const data = await response.json();
+    const maxNumber = getRandomInt(data.length);
+    quoteText.innerHTML = "";
+    quoteText.innerHTML = data[maxNumber].text;
+    author.innerHTML = data[maxNumber].author;
+  } catch {}
+}
